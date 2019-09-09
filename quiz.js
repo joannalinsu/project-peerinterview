51efe7902bda27db01a70699dc3e6ff78916f92e
Su Lin <sulinjoanna@gmail.com>
2019-09-09 10:30:52 -0400

    001

---
+++ quiz.js
@@ -0,0 +1,129 @@
+(function() 
+ {
+  var allQuestions = [{
+    question: "What did Ike study when he was a undergrad?",
+    options: ["Communication", "Sociology", "French", "Spanish"],
+    answer: 2
+  }, {
+    question: "How many language can Ike speak?",
+    options: ["5", "4", "3", "2"],
+    answer: 1
+  }, {
+    question: "How many years has he spent in NYC?",
+    options: ["more than 10", "8", "5","just arrived"],
+    answer: 1
+  },{
+    question: "Which of the following is NOT the reason for Ike lives in this city?",
+    options: ["Friend", "Job opportunity", "Girlfriend/ Ex Girlfriend", "Family"],
+    answer: 3
+  }, {
+    question: "In which area had he spent years exploring?",
+    options: ["Culture report", "Education tech", "Public Relation", "Product Management"],
+    answer: 1
+  }];
+  
+  var quesCounter = 0;
+  var selectOptions = [];
+  var quizSpace = $('#quiz');
+    
+  nextQuestion();
+    
+  $('#next').click(function () 
+    {
+        chooseOption();
+        if (isNaN(selectOptions[quesCounter])) 
+        {
+            alert('choose one!');
+        } 
+        else 
+        {
+          quesCounter++;
+          nextQuestion();
+        }
+    });
+  
+  
+  function createElement(index) 
+    {
+        var element = $('<div>',{id: 'question'});
+        var header = $('<h2>Question No. ' + (index + 1) + ' :</h2>');
+        element.append(header);
+
+        var question = $('<p>').append(allQuestions[index].question);
+        element.append(question);
+
+        var radio = radioButtons(index);
+        element.append(radio);
+
+        return element;
+    }
+  
+  function radioButtons(index) 
+    {
+        var radioItems = $('<ul>');
+        var item;
+        var input = '';
+        for (var i = 0; i < allQuestions[index].options.length; i++) {
+          item = $('<li>');
+          input = '<input type="radio" name="answer" value=' + i + ' />';
+          input += allQuestions[index].options[i];
+          item.append(input);
+          radioItems.append(item);
+        }
+        return radioItems;
+  }
+  
+  function chooseOption() 
+    {
+        selectOptions[quesCounter] = +$('input[name="answer"]:checked').val();
+    }
+   
+  function nextQuestion() 
+    {
+        quizSpace.fadeOut(function() 
+            {
+              $('#question').remove();
+              if(quesCounter < allQuestions.length)
+                {
+                    var nextQuestion = createElement(quesCounter);
+                    quizSpace.append(nextQuestion).fadeIn();
+                    if (!(isNaN(selectOptions[quesCounter]))) 
+                    {
+                      $('input[value='+selectOptions[quesCounter]+']').prop('checked', true);
+                    }
+                    if(quesCounter === 1)
+                    {
+                      $('#prev').show();
+                    } 
+                    else if(quesCounter === 0)
+                    {
+                      $('#prev').hide();
+                      $('#next').show();
+                    }
+                }
+              else 
+                {
+                    var scoreRslt = displayResult();
+                    quizSpace.append(scoreRslt).fadeIn();
+                    $('#next').hide();
+                    $('#prev').hide();
+                }
+        });
+    }
+  
+  function displayResult() 
+    {
+        var score = $('<p>',{id: 'question'});
+        var correct = 0;
+        for (var i = 0; i < selectOptions.length; i++) 
+        {
+          if (selectOptions[i] === allQuestions[i].answer) 
+          {
+            correct++;
+          }
+        }
+        score.append('You scored ' + correct + ' out of ' +allQuestions.length);
+        return score;
+  }
+})();
+
---
+++ scroll.js
@@ -0,0 +1,49 @@
+(function() 
+ {
+// using d3 for convenience
+				var container = document.querySelector('#scroll');
+		var text = container.querySelector('.scroll__text');
+		var steps = text.querySelectorAll('.step');
+		// initialize the scrollama
+		var scroller = scrollama();
+		// scrollama event handlers
+		function handleStepEnter(response) {
+			// response = { element, direction, index }
+			console.log('enter', response);
+			// add to color to current step
+			response.element.classList.add('is-active');
+		}
+		function handleStepExit(response) {
+			// response = { element, direction, index }
+			console.log('exit', response);
+			// remove color from current step
+			response.element.classList.remove('is-active');
+		}
+		function handleStepProgress(response) {
+			console.log(response.progress)
+			d3.select(response.element).select('p').text(d3.format('.1%')(response.progress))
+		}
+		function init() {
+			// set random padding for different step heights (not required)
+			steps.forEach(function (step) {
+				var v = 100 + Math.floor(Math.random() * window.innerHeight / 4);
+				step.style.padding = v + 'px 0px';
+			});
+			// 1. setup the scroller with the bare-bones options
+			// this will also initialize trigger observations
+			// 3. bind scrollama event handlers (this can be chained like below)
+			scroller.setup({
+				step: '.scroll__text .step',
+				debug: true,
+				progress: true,
+			})
+				.onStepEnter(handleStepEnter)
+				.onStepExit(handleStepExit)
+				.onStepProgress(handleStepProgress)
+			// setup resize event
+			window.addEventListener('resize', scroller.resize);
+		}
+		// kick things off
+		init();
+}
+		})();
